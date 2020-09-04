cdca5dc4e59d44006cbeef17c05e1f4e6c410f96
Austin Ramsdale <austin@skuintel.com>
2020-09-04 13:37:03 -0400

    Initial Commit

---
+++ app-cars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-cars
+//
+//  Created by Austin Ramsdale on 9/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app-carsTests/app_carsTests.swift
@@ -0,0 +1,33 @@
+//
+//  app_carsTests.swift
+//  app-carsTests
+//
+//  Created by Austin Ramsdale on 9/4/20.
+//
+
+import XCTest
+@testable import app_cars
+
+class app_carsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
